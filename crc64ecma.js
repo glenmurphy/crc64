d8f875c5b4d47f27028a8ccc3099ed8ced5808bb
Glen Murphy <glen@glenmurphy.com>
2021-01-07 15:21:53 -0800

    fix formatting

--- crc64ecma.js
+++ crc64ecma.js
@@ -7,17 +7,16 @@ function generate_crc64_table() {
   var c = 0n;
   var crc = 0n;
 
-	for (i = 0n; i < 256n; i++) {
-		crc = 0n;
-		c = i << 56n;
+  for (i = 0n; i < 256n; i++) {
+    crc = 0n;
+    c = i << 56n;
 
-		for (j = 0; j < 8; j++) {
-			if ((crc ^ c) & 0x8000000000000000n) {
+    for (j = 0; j < 8; j++) {
+      if ((crc ^ c) & 0x8000000000000000n)
         crc = (crc << 1n) ^ CRC64_ECMA182_POLY;
-      } else {
-        crc <<= 1n
-      }
-			c <<= 1n;
+      else
+        crc <<= 1n;
+      c <<= 1n;
     }
 
     crc64_table[i] = crc & mask;
@@ -32,13 +31,10 @@ export function crc64ecma(str) {
   for (var i = 0; i < str.length; i++) {
     var byte = BigInt(str.charCodeAt(i) & 0xFF);
     var byte56 = byte << 56n;
-
     crc = (crc ^ byte56);
-    
     var index = (crc >> 56n) & 0xFFn;
-
     crc = ((crc << 8n) ^ crc64_table[index]) & mask;
-    
+
   }
   return crc.toString(16);
 }
