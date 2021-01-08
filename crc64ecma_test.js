725fb3feb94ac9bf3c6d604c9b38fa3b877aaeff
Glen Murphy <glen@glenmurphy.com>
2021-01-08 08:20:46 -0800

    Add colors to tests

--- crc64ecma_test.js
+++ crc64ecma_test.js
@@ -2,7 +2,7 @@ import { crc64ecma } from './crc64ecma.js';
 
 function expect(b) {
   var line = (new Error).stack.split("\n")[2].split(":")[2];
-  console.log(`${line}: ${b ? 'PASS' : 'FAIL'}`);
+  console.log(`${line}: ${b ? '\u001b[32mPASS' : '\u001b[31mFAIL'}\u001b[0m`);
 }
 
 // You can test this using values from other implementations like
@@ -12,6 +12,6 @@ expect(crc64ecma('glen') == 'aa3a22c699bca958');
 expect(crc64ecma('cyclic redundancy check (CRC)') == '47c7fe019bf59b2d');
 expect(crc64ecma("!@#)!@# 123 312 0213\n\n120-e2e1") == '996d2d3d42d8ef34');
 
-// The online JS tools expect '6cff476e9ada3b3b' as they appear to throw away charCodeAts
-// greater than 255 (they don't appear to handle UTF-16); need to investigate more
+// The online JS tools expect '6cff476e9ada3b3b' as it looks like they throw away charCodes
+// greater than 255 (so they don't appear to handle UTF-16); need to investigate more
 expect(crc64ecma("in the sea o🇸🇬f human dignity, the😄re is only\n\ncheese") == '2b9dec10da882bf1');
